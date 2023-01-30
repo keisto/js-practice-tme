f5c27612baafaf2d2567ec7bd2997224d79e037d
Tony Keiser <keisertony@gmail.com>
2023-01-30 12:19:15 -0800

    App complete added readme and screenshot

---
+++ README.md
@@ -0,0 +1,10 @@
+## Simple Test Framework
+
+```shell
+# By default it will search for all files ending with 'test.js'
+tme
+```
+
+Note: Run npm link to get this to work locally.
+
+![app-screenshot.png](app-screenshot.png)
--- sampleWebProject/test/app.test.js
+++ sampleWebProject/test/app.test.js
@@ -18,3 +18,15 @@ it('shows a success message with a valid email', async () => {
   const h1 = dom.window.document.querySelector('h1')
   assert.strictEqual(h1.innerText, 'Looks good!')
 })
+
+it('shows a error message with an invalid email', async () => {
+  const dom = await render('index.html')
+  const input = dom.window.document.querySelector('input')
+  input.value = 'whateva'
+  dom.window.document
+    .querySelector('form')
+    .dispatchEvent(new dom.window.Event('submit'))
+
+  const h1 = dom.window.document.querySelector('h1')
+  assert.strictEqual(h1.innerText, 'Invalid email.')
+})
